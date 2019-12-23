5cf43bbb4702b7a390de2687d4910cd39aa82db2
Alvaro Zambrana <alvarozs.js@gmail.com>
2019-12-23 07:46:06 -0400

    PUI improve style on toppings

--- app/containers/ToppingsPage/ToppingsPage.js
+++ app/containers/ToppingsPage/ToppingsPage.js
@@ -8,20 +8,22 @@ import { Helmet } from 'react-helmet';
 import './style.scss';
 import PropTypes from 'prop-types';
 import AddToppingForm from 'components/AddToppingForm';
+import { List } from 'semantic-ui-react';
 import { useFetching } from '../../helpers/hooks';
 
 export const EmptyList = () => (<li><p>There is no toppings on the system.</p></li>);
-export const Topping = ({ topping }) => (<li key={topping.id}><p>{topping.name}</p></li>);
+export const Topping = ({ topping }) => (<p key={topping.id}>{topping.name}</p>);
 
 // candidate to be moved to its own js file for maintainability purpose
 export const ToppingList = ({ toppings }) => (
   <>
     <h1>Toppings</h1>
-    <ul>
+    <List>
       {(toppings.length === 0)
         ? <EmptyList />
-        : toppings.map((topping) => (<Topping key={`topping${topping.toppingId}`} topping={topping} />))}
-    </ul>
+        : toppings.map((topping) =>
+          (<List.Item><Topping key={`topping${topping.toppingId}`} topping={topping} /></List.Item>))}
+    </List>
   </>
 );
 
